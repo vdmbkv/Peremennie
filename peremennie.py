3dcb041676375afbd261a0db5fb99ca698fd064e
vdmbkv <lpnight2@gmail.com>
2024-10-13 17:02:53 +0700

    Add files via upload

---
+++ peremennie.py
@@ -0,0 +1,6 @@
+completed_homework = 12
+hours_spent = 1.5
+course_name = 'Python'
+time_spent_on_one_task = (hours_spent / completed_homework)
+
+print('Курс:', course_name, ',', 'всего задач:', completed_homework, ',', 'затрачено часов:', hours_spent, ',', 'среднее время выполнения', time_spent_on_one_task)
